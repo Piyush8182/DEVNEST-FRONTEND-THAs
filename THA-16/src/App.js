db7d69733aaedee4b5452845c18eb1eb26061205
Piyush8182 <61621595+Piyush8182@users.noreply.github.com>
2021-07-10 19:35:18 +0530

    Devnest THA

---
+++ THA-16/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Card from './Card'
+import Board from './Board';
+import './styles.css'
+function App() {
+  return (
+    <div className="app">
+    <Card />
+    <Board />
+    </div>
+  );
+}
+
+export default App;
